408e1f3d4e0753722aeac713015f4ef9b22389a1
PreslavMihaylov <preslav@pmihaylov.com>
2017-12-02 20:04:06 +0200

    added initial install script

---
+++ install-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# add google repository to apt
+echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google.list 
+
+# add google repository sign key
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+sudo apt-get update
+
+# Installations
+sudo apt-get install -yy vim
+sudo apt-get install -yy deluge # torrent tracker
+sudo apt-get install -yy google-chrome-stable
+sudo apt-get install -yy git
+sudo apt-get install -yy gcc
+
+# git configurations
+git config --global user.email "preslav@pmihaylov.com"
+git config --global user.name "PreslavMihaylov"
+
