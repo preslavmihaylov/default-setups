07a31bc1b9ee2fea6230a0999519d9ff994f6fe8
Preslav Mihaylov <preslav@pmihaylov.com>
2019-07-03 09:18:26 +0300

    delete install artifacts after installing golang

--- install_go.sh
+++ install_go.sh
@@ -9,3 +9,5 @@ wget https://dl.google.com/go/${GO_ARC}
 sudo tar xvzf ${GO_ARC} -C ${GO_LOC}
 sudo mv ${GO_LOC}/go ${GO_LOC}/${GO_FOLDER}
 sudo ln -s ${GO_LOC}/${GO_FOLDER} ${GO_LOC}/go
+
+rm -rf ${GO_ARC}
