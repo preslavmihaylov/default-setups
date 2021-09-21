8dce0996a7f5f2a46d2a727aae276ff7802ef0d8
preslavmihaylov <preslav@pmihaylov.com>
2021-09-21 18:08:25 +0300

    up-to-date the mac install script

--- mac/install.sh
+++ mac/install.sh
@@ -19,15 +19,17 @@ brew install tmux
 brew install wget
 brew install golang
 brew install the_silver_searcher # ag command
-brew cask install alacritty
 brew install alacritty
+brew install ffmpeg
+brew install tmux
+brew install watchman
+brew install jq
 
 # install apps from app store
 mas install 462054704 # Microsoft Word
 mas install 462058435 # Microsoft Excel
 mas install 462062816 # Microsoft PowerPoint
 mas install 784801555 # Microsoft OneNote
-mas install 526298438 # Lightshot Screenshot
 mas install 1456462087 # All-in-one Messenger
 
 # install fzf
@@ -35,30 +37,10 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # configure GOPATH
-mkdir -p ~/programming/go/src ~/programming/go/pkg ~/programming/go/bin
+mkdir -p ~/prg/go/src ~/prg/go/pkg ~/prg/go/bin
 
 # install go binaries
-go install github.com/zmb3/gogetdoc
-go install golang.org/x/tools/cmd/guru
 go install golang.org/x/tools/gopls@latest
-go install github.com/davidrjenni/reftools/cmd/fillstruct
-go install github.com/rogpeppe/godef
-go install github.com/fatih/motion
-go install github.com/kisielk/errcheck
-go install github.com/go-delve/delve/cmd/dlv
-go install github.com/mdempsky/gocode
-go install github.com/josharian/impl
-go install github.com/golangci/golangci-lint/cmd/golangci-lint
-go install github.com/jstemmer/gotags
-go install golang.org/x/tools/cmd/gorename
-go install golang.org/x/tools/cmd/goimports
-go install github.com/stamblerre/gocode
-go install github.com/fatih/gomodifytags
-go install honnef.co/go/tools/cmd/keyify
-go install golang.org/x/lint/golint
-go install github.com/koron/iferr
-go install github.com/klauspost/asmfmt/cmd/asmfmt
-go install github.com/alecthomas/gometalinter
 
 # git configuration
 git config --global user.email "preslav@pmihaylov.com"
@@ -87,9 +69,6 @@ sudo scutil --set ComputerName kingslanding
 
 # setup key repeating
 defaults write -g ApplePressAndHoldEnabled -bool false
-
-# Add Bulgarian Phonetic to input sources
-defaults write com.apple.HIToolbox AppleEnabledInputSources -array-add '<dict><key>InputSourceKind</key><string>Keyboard Layout</string><key>KeyboardLayout ID</key><integer>19529</integer><key>KeyboardLayout Name</key><string>Bulgarian - Phonetic</string></dict>'
 
 # install dotfiles
 git clone --recurse-submodules https://github.com/preslavmihaylov/dotfiles
